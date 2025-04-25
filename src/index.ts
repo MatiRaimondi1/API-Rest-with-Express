f77d2af6170987660ac446828ab63d99c7026486
Matias Raimondi <matiraimondi1@gmail.com>
2025-04-25 20:08:48 -0300

    Update index.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import app from './app';
+import { AppDataSource } from './database';
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor hosteado en http://localhost:${PORT}`);
-});
+AppDataSource.initialize().then(() => {
+    app.listen(3000, () => {
+        console.log(`Servidor hosteado en http://localhost:3000`);
+    });
+}).catch(error => console.error("Error al conectarse a la base de datos", error));
